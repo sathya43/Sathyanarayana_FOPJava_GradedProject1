e1f96bcde8dfc8aa70ac3f1377a04f4f77377567
sathya gundoji <56188753+sathya43@users.noreply.github.com>
2022-10-03 10:51:37 +0530

    Updated typo

--- src/com/gl/gradedassessment/Models/AdminDepartment.java
+++ src/com/gl/gradedassessment/Models/AdminDepartment.java
@@ -13,7 +13,7 @@ public class AdminDepartment extends SuperDepartment {
 
 	@Override
 	public String getTodaysWork() {
-		return "Complete your documents Submission";
+		return "Complete your documents submission";
 	}
 
 	@Override
